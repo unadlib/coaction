4de18e5dbbfe058d5bdb3623e606a6cfeb340656
unadlib <unadlib@gmail.com>
2024-07-28 14:04:29 +0800

    fix(base): fix issue

--- packages/core/test/index.test.ts
+++ packages/core/test/index.test.ts
@@ -1,5 +1,30 @@
 import { create } from '../src';
 
 test('base', () => {
-  //
+  const store = create((store) => ({
+    name: 'test',
+    count: 0,
+    increment() {
+      store.setState((draft) => {
+        draft.count += 1;
+      });
+    }
+  }));
+  expect(store.getState()).toMatchInlineSnapshot(`
+{
+  "count": 0,
+  "increment": [Function],
+  "name": "test",
+}
+`);
+  const fn = jest.fn();
+  store.subscribe(fn);
+  store.getState().increment();
+  expect(store.getState()).toMatchInlineSnapshot(`
+{
+  "count": 1,
+  "increment": [Function],
+  "name": "test",
+}
+`);
 });
