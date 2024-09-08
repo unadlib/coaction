d8f65d4df34351487c3aa20de0b9bdacb43a666c
unadlib <unadlib@gmail.com>
2024-09-08 12:07:04 +0800

    chore(example): update

--- examples/vanilla-base/src/counter.ts
+++ examples/vanilla-base/src/counter.ts
@@ -1,9 +1,31 @@
+import { create } from 'coaction';
+import { useStore } from './store';
+
+// @ts-ignore
+const { increment } = useStore();
+
+// const worker = new Worker(new URL('./store.ts', import.meta.url), { type: 'module' });
+
+// const useWorkerStore = useStore(worker);
+// const { increment } = useWorkerStore();
+
+// document.getElementById('increment').addEventListener('click', increment);
+// document.getElementById('decrement').addEventListener('click', decrement);
+
+// useWorkerStore.subscribe(
+//   () => useWorkerStore.count,
+//   () => {
+//     document.getElementById('count').innerText = useWorkerStore.count;
+//   }
+// );
+
+console.log('create');
+
 export function setupCounter(element: HTMLButtonElement) {
-  let counter = 0;
-  const setCounter = (count: number) => {
-    counter = count;
-    element.innerHTML = `count is ${counter}`;
-  };
-  element.addEventListener('click', () => setCounter(counter + 1));
-  setCounter(0);
+  console.log('useStore.getState().counter', useStore.getState().count);
+  useStore.subscribe(() => {
+    element.innerHTML = `count is ${useStore.getState().count}`;
+  });
+  element.addEventListener('click', () => useStore.getState().increment());
+  element.innerHTML = `count is ${useStore.getState().count}`;
 }
