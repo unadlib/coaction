ac14ad64fee025771f743a1a5a6cf4d09e7eddeb
unadlib <unadlib@gmail.com>
2024-09-13 01:18:32 +0800

    feat(mobx): implement patches for mobx

--- packages/coaction-mobx/test/index.test.ts
+++ packages/coaction-mobx/test/index.test.ts
@@ -1,5 +1,67 @@
-// import { add } from '../src/index.ts';
+import { create } from 'coaction';
+import { makeAutoObservable, autorun } from 'mobx';
+import { mobx } from '../src';
 
 test('base', () => {
-  // expect(add(1, 2)).toBe(3);
+  const store = makeAutoObservable({
+    name: 'test',
+    count: 0,
+    increment() {
+      this.count += 1;
+    }
+  });
+  autorun(() => {
+    console.log(store.count);
+  });
+  expect(store.count).toBe(0);
+  expect(store.increment).toBeInstanceOf(Function);
+  expect(store.name).toBe('test');
+  store.increment();
+  expect(store.count).toBe(1);
+});
+
+test.only('base', () => {
+  const store = makeAutoObservable({
+    name: 'test',
+    count: 0,
+    increment() {
+      this.count += 1;
+    }
+  });
+  autorun(() => {
+    console.log(store.count);
+  });
+  const useStore = create(mobx(store));
+  // TODO: fix this
+  // @ts-ignore
+  const { count, increment, name } = useStore();
+  expect(count).toBe(0);
+  expect(increment).toBeInstanceOf(Function);
+  expect(name).toBe('test');
+  expect(useStore.getState()).toMatchInlineSnapshot(`
+  {
+    "count": 0,
+    "increment": [Function],
+    "name": "test",
+  }
+  `);
+  const fn = jest.fn();
+  useStore.subscribe(fn);
+  // @ts-ignore
+  useStore.getState().increment();
+  expect(useStore.getState()).toMatchInlineSnapshot(`
+  {
+    "count": 1,
+    "increment": [Function],
+    "name": "test",
+  }
+  `);
+  increment();
+  expect(useStore.getState()).toMatchInlineSnapshot(`
+  {
+    "count": 2,
+    "increment": [Function],
+    "name": "test",
+  }
+  `);
 });
