cac662de16c75221c2a0003169b391cae5ab64fc
unadlib <unadlib@gmail.com>
2024-10-01 02:22:09 +0800

    test(mobx): add coaction-mobx test

--- packages/coaction-mobx/test/index.test.ts
+++ packages/coaction-mobx/test/index.test.ts
@@ -1,4 +1,5 @@
 import { makeAutoObservable, autorun } from 'mobx';
+import { createTransport, mockPorts } from 'data-transport';
 import { createWithMobx } from '../src';
 
 test('base', () => {
@@ -17,6 +18,98 @@ test('base', () => {
   expect(store.name).toBe('test');
   store.increment();
   expect(store.count).toBe(1);
+});
+
+test('worker', async () => {
+  const ports = mockPorts();
+  const serverTransport = createTransport('WorkerInternal', ports.main);
+  const clientTransport = createTransport('WorkerMain', ports.create());
+
+  const counter = () => ({
+    name: 'test',
+    count: 0,
+    increment() {
+      this.count += 1;
+    }
+  });
+  const useServerStore = createWithMobx(counter, {
+    transport: serverTransport,
+    workerType: 'WorkerInternal'
+  });
+  // TODO: fix this
+  // @ts-ignore
+  const { count, increment, name } = useServerStore();
+  expect(count).toBe(0);
+  expect(increment).toBeInstanceOf(Function);
+  expect(name).toBe('test');
+  expect(useServerStore.getState()).toMatchInlineSnapshot(`
+{
+  "count": 0,
+  "increment": [Function],
+  "name": "test",
+}
+`);
+  const fn = jest.fn();
+  useServerStore.subscribe(fn);
+  useServerStore.getState().increment();
+  expect(useServerStore.getState()).toMatchInlineSnapshot(`
+{
+  "count": 1,
+  "increment": [Function],
+  "name": "test",
+}
+`);
+  increment.call(useServerStore.getState());
+  expect(useServerStore.getState()).toMatchInlineSnapshot(`
+{
+  "count": 2,
+  "increment": [Function],
+  "name": "test",
+}
+`);
+  //   {
+  //     const useClientStore = createWithMobx(counter)({
+  //       transport: clientTransport,
+  //       workerType: 'WorkerMain'
+  //     });
+
+  //     await new Promise((resolve) => {
+  //       clientTransport.onConnect(() => {
+  //         setTimeout(resolve);
+  //       });
+  //     });
+
+  //     // @ts-ignore
+  //     const { count, increment, name } = useClientStore();
+  //     expect(count).toBe(2);
+  //     expect(increment).toBeInstanceOf(Function);
+  //     expect(name).toBe('WorkerInternal');
+  //     expect(useClientStore.getState()).toMatchInlineSnapshot(`
+  // {
+  //   "count": 2,
+  //   "increment": [Function],
+  //   "name": "WorkerInternal",
+  // }
+  // `);
+  //     const fn = jest.fn();
+  //     useClientStore.subscribe(fn);
+  //     useClientStore.getState().increment();
+  //     expect(useClientStore.getState()).toMatchInlineSnapshot(`
+  // {
+  //   "count": 3,
+  //   "increment": [Function],
+  //   "name": "WorkerInternal",
+  // }
+  // `);
+  //     increment();
+  //     expect(useClientStore.getState()).toMatchInlineSnapshot(`
+  // {
+  //   "count": 4,
+  //   "increment": [Function],
+  //   "name": "WorkerInternal",
+  // }
+  // `);
+  //   }
 });
 
 test('base', () => {
