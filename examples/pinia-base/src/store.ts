4f9055e06b2e8a44cf06cfab7a83158d683900db
unadlib <unadlib@gmail.com>
2024-10-03 02:46:06 +0800

    fix(pinia): fix pinia store

--- examples/pinia-base/src/store.ts
+++ examples/pinia-base/src/store.ts
@@ -1,5 +1,4 @@
-import { defineStore } from 'pinia';
-import { createWithPinia } from '@coaction/pinia';
+import { createWithPinia, defineStore } from '@coaction/pinia';
 
 export const useStore = createWithPinia(() =>
   defineStore('counter', {
@@ -7,15 +6,18 @@ export const useStore = createWithPinia(() =>
       count: 0
     }),
     getters: {
+      // @ts-ignore
       doubleCount: (state) => state.count * 2
     },
     actions: {
       // since we rely on `this`, we cannot use an arrow function
       increment() {
+        // @ts-ignore
         this.count++;
       }
     }
   })
 );
 
+// @ts-ignore
 globalThis.useStore = useStore;
