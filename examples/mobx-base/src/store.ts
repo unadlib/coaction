626e8c8ec894f703a14976546d6ebaf825ce57f9
unadlib <unadlib@gmail.com>
2024-11-10 02:32:47 +0800

    chore(example): fix example

--- examples/mobx-base/src/store.ts
+++ examples/mobx-base/src/store.ts
@@ -1,40 +1,52 @@
-import { create } from 'coaction';
+import { create, Slices } from 'coaction';
 import { bindMobx } from '@coaction/mobx';
 import { makeAutoObservable } from 'mobx';
 
+type Counter = Slices<
+  {
+    counter: {
+      count: number;
+      increment1: () => void;
+      increment: () => Promise<void>;
+    };
+  },
+  'counter'
+>;
+
+const counter: Counter = (set) =>
+  makeAutoObservable(
+    bindMobx({
+      name: 'test',
+      count: 0,
+      get double() {
+        return this.count * 2;
+      },
+      increment1() {
+        this.count += 1;
+      },
+      async increment() {
+        set(() => {
+          this.increment1();
+        });
+        set(() => {
+          this.count += 1;
+        });
+        set(() => {
+          this.count += 1;
+        });
+        set(() => {
+          this.increment1();
+        });
+        set((state) => {
+          state.counter.increment1();
+        });
+      }
+    })
+  );
+
 export const useStore = create(
   {
-    counter: (set) =>
-      makeAutoObservable(
-        bindMobx({
-          name: 'test',
-          count: 0,
-          get double() {
-            return this.count * 2;
-          },
-          a() {
-            this.count += 1;
-            console.log('count', this.count, this.double);
-          },
-          async increment() {
-            set(() => {
-              this.a();
-            });
-            set(() => {
-              this.count += 1;
-            });
-            set(() => {
-              this.count += 1;
-            });
-            set(() => {
-              this.a();
-            });
-            set((state) => {
-              state.counter.a();
-            });
-          }
-        })
-      )
+    counter
   },
   {
     enablePatches: true
