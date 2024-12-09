c7d866b4770f928625c134470698092f9c5d549c
unadlib <unadlib@gmail.com>
2024-12-09 23:27:01 +0800

    fix(coaction): update

--- packages/coaction-react/src/index.ts
+++ packages/coaction-react/src/index.ts
@@ -1,8 +1,8 @@
 import { create as createVanilla, type Slice, type Store } from 'coaction';
 import { useSyncExternalStore } from 'use-sync-external-store/shim';
 
-export const create = (createState: any) => {
-  const store = createVanilla(createState);
+export const create = (createState: any, options: any) => {
+  const store = createVanilla(createState, options);
   return (selector: any) =>
     useSyncExternalStore(
       store.subscribe,
