fb719196b917ae5648195d46fd45bbe719af6693
unadlib <unadlib@gmail.com>
2024-12-10 00:10:25 +0800

    chore(react-base): add react example

--- packages/coaction-react/src/index.ts
+++ packages/coaction-react/src/index.ts
@@ -1,12 +1,20 @@
 import { create as createVanilla, type Slice, type Store } from 'coaction';
 import { useSyncExternalStore } from 'use-sync-external-store/shim';
 
+export * from 'coaction';
+
 export const create = (createState: any, options: any) => {
   const store = createVanilla(createState, options);
-  return (selector: any) =>
-    useSyncExternalStore(
-      store.subscribe,
-      () => selector(store.getState()),
-      () => selector(store.getInitialState())
-    );
+  const { name, ..._store } = store;
+  return Object.assign(
+    {
+      [name]: (selector: any) =>
+        useSyncExternalStore(
+          store.subscribe,
+          () => selector(store.getState()),
+          () => selector(store.getInitialState())
+        )
+    }[name],
+    _store
+  ) as any;
 };
