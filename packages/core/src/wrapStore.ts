ed964e9f10b99b1341b5bec43455d9a2ac84a920
unadlib <unadlib@gmail.com>
2026-02-20 02:52:20 +0800

    refactor(core): make wrapStore generic and strongly typed

--- packages/core/src/wrapStore.ts
+++ packages/core/src/wrapStore.ts
@@ -3,15 +3,15 @@ import type { Store, StoreReturn } from './interface';
 /**
  * wrapStore is a function to wrap the store and return function to get the state with store.
  */
-export const wrapStore = (
-  store: Store<any>,
-  getState: (...args: any[]) => any = () => store.getState()
+export const wrapStore = <T extends object>(
+  store: Store<T>,
+  getState: (...args: unknown[]) => T = () => store.getState()
 ) => {
   const { name, ..._store } = store;
   return Object.assign(
     {
-      [name]: (...args: any[]) => getState(...args)
+      [name]: (...args: unknown[]) => getState(...args)
     }[name],
     _store
-  ) as StoreReturn<any>;
+  ) as StoreReturn<T>;
 };
