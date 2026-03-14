54209c0018f3bda5ac44baeb731c86ac20ea119c
unadlib <unadlib@gmail.com>
2026-03-14 08:37:02 +0800

    refactor(core): split getRawState responsibilities

---
+++ packages/core/src/getRawStateLocalAction.ts
@@ -0,0 +1,124 @@
+import {
+  create as createWithMutative,
+  type Draft,
+  isDraft,
+  type Patches
+} from 'mutative';
+import { handleDraft } from './asyncClientStore';
+import type {
+  ClientStoreOptions,
+  CreateState,
+  MiddlewareStore,
+  StoreOptions
+} from './interface';
+import type { Internal } from './internal';
+import { uuid } from './utils';
+
+type CreateLocalActionOptions<T extends CreateState> = {
+  fn: (...args: unknown[]) => unknown;
+  internal: Internal<T>;
+  key: string;
+  options: StoreOptions<T> | ClientStoreOptions<T>;
+  store: MiddlewareStore<T>;
+  sliceKey?: string;
+};
+
+const getActionTarget = <T extends CreateState>(
+  store: MiddlewareStore<T>,
+  sliceKey?: string
+) => {
+  return sliceKey ? store.getState()[sliceKey] : store.getState();
+};
+
+export const createLocalAction = <T extends CreateState>({
+  fn,
+  internal,
+  key,
+  options,
+  store,
+  sliceKey
+}: CreateLocalActionOptions<T>) => {
+  return (...args: unknown[]) => {
+    let actionId: string | undefined;
+    let done: ((result: any) => void) | undefined;
+    if (store.trace) {
+      actionId = uuid();
+      store.trace({
+        method: key,
+        parameters: args,
+        id: actionId,
+        sliceKey
+      });
+      done = (result: any) => {
+        store.trace!({
+          method: key,
+          id: actionId!,
+          result,
+          sliceKey
+        });
+      };
+    }
+    const enablePatches =
+      store.transport ?? (options as StoreOptions<T>).enablePatches;
+    if (internal.mutableInstance && !internal.isBatching && enablePatches) {
+      let result: any;
+      const handleResult = (isDrafted?: boolean) => {
+        handleDraft(store, internal);
+        if (isDrafted) {
+          internal.backupState = internal.rootState;
+          const [draft, finalize] = createWithMutative(internal.rootState, {
+            enablePatches: true
+          });
+          internal.finalizeDraft = finalize as () => [T, Patches, Patches];
+          internal.rootState = draft as Draft<T>;
+        }
+      };
+      const isDrafted = isDraft(internal.rootState);
+      if (isDrafted) {
+        handleResult();
+      }
+      internal.backupState = internal.rootState;
+      const [draft, finalize] = createWithMutative(internal.rootState, {
+        enablePatches: true
+      });
+      internal.finalizeDraft = finalize as () => [T, Patches, Patches];
+      internal.rootState = draft as Draft<T>;
+      let asyncResult: Promise<unknown> | undefined;
+      try {
+        result = fn.apply(getActionTarget(store, sliceKey), args);
+        if (result instanceof Promise) {
+          asyncResult = result;
+        }
+      } finally {
+        if (asyncResult) {
+          // if (process.env.NODE_ENV === 'development') {
+          //   console.warn(
+          //     'It will be combined with the next state in the async function.'
+          //   );
+          // }
+        } else {
+          handleResult(isDrafted);
+        }
+      }
+      if (asyncResult) {
+        return asyncResult.finally(() => {
+          const result = handleResult(isDrafted);
+          done?.(result);
+          return result;
+        });
+      }
+      done?.(result);
+      return result;
+    }
+    if (internal.mutableInstance && internal.actMutable) {
+      const result = internal.actMutable(() => {
+        return fn.apply(getActionTarget(store, sliceKey), args);
+      });
+      done?.(result);
+      return result;
+    }
+    const result = fn.apply(getActionTarget(store, sliceKey), args);
+    done?.(result);
+    return result;
+  };
+};
---
+++ packages/core/src/getRawStateStateProperty.ts
@@ -0,0 +1,68 @@
+import { Computed, createSelectorWithArray } from './computed';
+import type { CreateState, Store } from './interface';
+import type { Internal } from './internal';
+
+type PrepareStateDescriptorOptions<T extends CreateState> = {
+  descriptor: PropertyDescriptor;
+  internal: Internal<T>;
+  key: string;
+  rawState: Record<string, any>;
+  sliceKey?: string;
+};
+
+export const prepareStateDescriptor = <T extends CreateState>({
+  descriptor,
+  internal,
+  key,
+  rawState,
+  sliceKey
+}: PrepareStateDescriptorOptions<T>) => {
+  const isComputed = descriptor.value instanceof Computed;
+  if (internal.mutableInstance) {
+    Object.defineProperty(rawState, key, {
+      get: () => internal.mutableInstance[key],
+      set: (value) => {
+        internal.mutableInstance[key] = value;
+      },
+      enumerable: true
+    });
+  } else if (!isComputed) {
+    rawState[key] = descriptor.value;
+  }
+
+  if (isComputed) {
+    if (internal.mutableInstance) {
+      throw new Error('Computed is not supported with mutable instance');
+    }
+    // manually handle computed property
+    const { deps, fn } = descriptor.value as Computed;
+    const depsCallbackSelector = createSelectorWithArray(
+      // the root state should be updated, and the computed property will be updated.
+      () => [internal.rootState],
+      () => {
+        return deps(internal.module as Store<T>['getState']);
+      }
+    );
+    const selector = createSelectorWithArray(
+      (that) => depsCallbackSelector.call(that),
+      fn
+    );
+    descriptor.get = function () {
+      return selector.call(this);
+    };
+  } else if (sliceKey) {
+    descriptor.get = () => (internal.rootState as any)[sliceKey][key];
+    descriptor.set = (value: unknown) => {
+      (internal.rootState as any)[sliceKey][key] = value;
+    };
+  } else {
+    descriptor.get = () => (internal.rootState as any)[key];
+    descriptor.set = (value: unknown) => {
+      (internal.rootState as any)[key] = value;
+    };
+  }
+
+  // handle state property
+  delete descriptor.value;
+  delete descriptor.writable;
+};
